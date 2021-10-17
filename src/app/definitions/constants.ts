8c46622b00f47ecdb11412a37fe6fbfadc3f4c74
SarvagyaS <sarvagya.saxenaa@gmail.com>
2021-10-17 17:58:21 +0530

    wip

--- src/app/definitions/constants.ts
+++ src/app/definitions/constants.ts
@@ -1,7 +1,7 @@
 export class Constants{
-  //public static baseUrl: string = "https://localhost:44316";
-  public static baseUrl: string = "http://43.240.64.140:8002";
+  public static baseUrl: string = "https://localhost:44316";
+  //public static baseUrl: string = "http://43.240.64.140:8002";
 
-   //public static apiBaseUrl: string = "https://localhost:44316/api";
-   public static apiBaseUrl: string = "http://43.240.64.140:8002/api";
+   public static apiBaseUrl: string = "https://localhost:44316/api";
+   //public static apiBaseUrl: string = "http://43.240.64.140:8002/api";
 }
